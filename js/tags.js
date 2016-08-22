ef98c8975beaa0e07948c2cc8941c20f46e30813
spaghettic0der <max.wittig95@gmail.com>
2016-08-22 23:27:38 +0200

    better code style

--- js/tags.js
+++ js/tags.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+$(document).ready(function ()
+{
 
     var checkBoxContainer = document.getElementById("checkBoxContainer");
     var div = checkBoxContainer.getElementsByTagName("div")[0];
@@ -6,7 +7,8 @@ $(document).ready(function () {
     if (saveProjectArray == null)
         saveProjectArray = [];
 
-    for (var i = 0; i < saveProjectArray.length; i++) {
+    for (var i = 0; i < saveProjectArray.length; i++)
+    {
         var project = saveProjectArray[i];
         var tableRow = document.getElementById("row_" + project.name);
         var input = document.createElement("input");
@@ -18,9 +20,12 @@ $(document).ready(function () {
 
     }
 
-    function setProjectTags(projectName, tags) {
-        for (var i = 0; i < saveProjectArray.length; i++) {
-            if (saveProjectArray[i].name == projectName) {
+    function setProjectTags(projectName, tags)
+    {
+        for (var i = 0; i < saveProjectArray.length; i++)
+        {
+            if (saveProjectArray[i].name == projectName)
+            {
                 var projectObject = saveProjectArray[i];
                 projectObject.tags = tags;
                 break;
@@ -29,13 +34,15 @@ $(document).ready(function () {
         localStorage.setItem("saveProjectArray", JSON.stringify(saveProjectArray));
     }
 
-    $('input').on('itemAdded', function (event) {
+    $('input').on('itemAdded', function (event)
+    {
         var projectName = this.id.split("_")[1];
         var items = $(this).materialtags('items');
         setProjectTags(projectName, items);
     });
 
-    $('input').on('itemRemoved', function (event) {
+    $('input').on('itemRemoved', function (event)
+    {
         var projectName = this.id.split("_")[1];
         var items = $(this).materialtags('items');
         setProjectTags(projectName, items);
